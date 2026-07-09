49ba80e113464cc2093f1e039dc40106b656275e
Exerra <exerra@exerra.xyz>
2026-07-10 00:08:37 +0300

    feat: add artist lookup support and parse relationships
    Implemented support for fetching artists by ID, updated search types to include Artist definitions, and enhanced the generic response parser to include resource relationships.

---
+++ src/types/artist.ts
@@ -0,0 +1,43 @@
+import type { GenericRelationship } from "./relationships";
+
+export interface ArtistRaw {
+    id:         string;
+    type:       string;
+    href:       string;
+    attributes: Attributes;
+    relationships: ArtistRelationships;
+}
+
+export type Artist = {
+    id:         string;
+} & Attributes & { relationships: ArtistRelationships }
+
+export interface ArtistRelationships {
+    albums: GenericRelationship;
+}
+
+export interface Attributes {
+    artwork:        Artwork;
+    editorialNotes: EditorialNotes;
+    genreNames:     string[];
+    name:           string;
+    url:            string;
+}
+
+export interface Artwork {
+    bgColor:    string;
+    height:     number;
+    textColor1: string;
+    textColor2: string;
+    textColor3: string;
+    textColor4: string;
+    url:        string;
+    width:      number;
+}
+
+export interface EditorialNotes {
+    name:     string;
+    short:    string;
+    standard: string;
+    tagline:  string;
+}
