4b68cddd8e8531a57cefbefb965a6ea331c1b519
Exerra <exerra@exerra.xyz>
2026-07-14 00:38:11 +0300

    refactor(me): move history props to shared types
    Moved MeHistoryGenericProps and MeHistoryRecentlyPlayedTracksProps definitions from src/resources/me/history.ts to the new src/types/me.ts file to improve maintainability and import consistency.

--- src/resources/me/history.ts
+++ src/resources/me/history.ts
@@ -2,20 +2,7 @@ import type { FetchAPI, MusicKitResultWrapper } from "../..";
 import type { Song, SongRaw } from "../../types/song";
 import type { HeavyRotationItem, HeavyRotationItemRaw } from "../../types/heavyrotation";
 import { flattenItem } from "../../util/flatten";
-
-// https://developer.apple.com/documentation/applemusicapi/get-v1-me-recent-played-tracks#query-parameters
-type MeHistoryGenericProps = {
-    l?: string;
-    include?: string[];
-    limit?: number;
-    offset?: number;
-    extend?: string[];
-}
-
-type MeHistoryRecentlyPlayedTracksProps = MeHistoryGenericProps & {
-    limit?: 1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30; // Default - 30
-    types?:("library-music-videos" | "library-songs" | "music-videos" | "songs")[];
-}
+import type { MeHistoryRecentlyPlayedTracksProps, MeHistoryGenericProps } from "../../types/me";
 
 export class MeHistoryResource {
     constructor(
